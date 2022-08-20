e4382203002ca2830322df3d0a35c49392a5b3ec
Carlos Santander <carlos.santander@gmail.com>
2022-08-20 18:31:54 -0500

    challenge complete

--- src/components/pages/Exercise01/index.js
+++ src/components/pages/Exercise01/index.js
@@ -7,120 +7,117 @@
  * 4. Apply discount rules. You have an array of offers with discounts depending of the combination of movie you have in your cart.
  * You have to apply all discounts in the rules array (discountRules).
  * Ex: If m: [1, 2, 3], it means the discount will be applied to the total when the cart has all that products in only.
- * 
+ *
  * You can modify all the code, this component isn't well designed intentionally. You can redesign it as you need.
  */
 
 import './assets/styles.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { movies, discountRules } from './data'
 
-export default function Exercise01 () {
-  const movies = [
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20
-    },
-    {
-      id: 2,
-      name: 'Minions',
-      price: 25
-    },
-    {
-      id: 3,
-      name: 'Fast and Furious',
-      price: 10
-    },
-    {
-      id: 4,
-      name: 'The Lord of the Rings',
-      price: 5
+export default function Exercise01() {
+  const [cart, setCart] = useState([])
+  const [discount, setDiscount] = useState(0)
+
+  // Check cart for apply discount
+  useEffect(() => {
+    setDiscount(0)
+    const cartIds = cart.map(c => c.id).sort()
+    discountRules.forEach(d => {
+      if (JSON.stringify(d.m.sort()) === JSON.stringify(cartIds)) {
+        setDiscount(d.discount)
+      }
+    })
+  }, [cart])
+
+  const addCart = movie => {
+    const existMovie = cart.some(c => c.id === movie.id)
+    // Only added a movie that is not in the cart.
+    if (!existMovie) {
+      setCart([
+        ...cart,
+        {
+          ...movie,
+          quantity: 1
+        }
+      ])
     }
-  ]
+  }
 
-  const discountRules = [
-    {
-      m: [3, 2],
-      discount: 0.25
-    },
-    {
-      m: [2, 4, 1],
-      discount: 0.5
-    },
-    {
-      m: [4, 2],
-      discount: 0.1
-    } 
-  ]
-
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20,
-      quantity: 2
+  const decrementQuantity = movie => {
+    // If quantity equals to 1, remove the movie from the cart
+    if (movie.quantity === 1) {
+      const newCart = cart.slice().filter(c => c.id !== movie.id)
+      setCart(newCart)
+    } else {
+      const newCart = cart.slice().map(c => {
+        if (c.id === movie.id) {
+          return { ...c, quantity: movie.quantity - 1 }
+        }
+        return c
+      })
+      setCart(newCart)
     }
-  ])
+  }
 
-  const getTotal = () => 0 // TODO: Implement this
+  const incrementQuantity = movie => {
+    const newCart = cart.slice().map(c => {
+      if (c.id === movie.id) {
+        return { ...c, quantity: movie.quantity + 1 }
+      }
+      return c
+    })
+    setCart(newCart)
+  }
+
+  const getTotal = () => {
+    const total = cart.reduce((t, m) => t + m.price * m.quantity, 0)
+    if (discount > 0) return total * discount
+    return total
+  }
 
   return (
     <section className="exercise01">
       <div className="movies__list">
         <ul>
-          {movies.map(o => (
-            <li className="movies__list-card">
+          {movies.map(m => (
+            <li key={m.id} className="movies__list-card">
               <ul>
-                <li>
-                  ID: {o.id}
-                </li>
-                <li>
-                  Name: {o.name}
-                </li>
-                <li>
-                  Price: ${o.price}
-                </li>
+                <li>ID: {m.id}</li>
+                <li>Name: {m.name}</li>
+                <li>Price: ${m.price}</li>
               </ul>
-              <button onClick={() => console.log('Add to cart', o)}>
-                Add to cart
-              </button>
+              <button onClick={() => addCart(m)}>Add to cart</button>
             </li>
           ))}
         </ul>
       </div>
       <div className="movies__cart">
         <ul>
-          {cart.map(x => (
-            <li className="movies__cart-card">
+          {cart.map(c => (
+            <li key={c.id} className="movies__cart-card">
               <ul>
-                <li>
-                  ID: {x.id}
-                </li>
-                <li>
-                  Name: {x.name}
-                </li>
-                <li>
-                  Price: ${x.price}
-                </li>
+                <li>ID: {c.id}</li>
+                <li>Name: {c.name}</li>
+                <li>Price: ${c.price}</li>
               </ul>
               <div className="movies__cart-card-quantity">
-                <button onClick={() => console.log('Decrement quantity', x)}>
-                  -
-                </button>
-                <span>
-                  {x.quantity}
-                </span>
-                <button onClick={() => console.log('Increment quantity', x)}>
-                  +
-                </button>
+                <button onClick={() => decrementQuantity(c)}>-</button>
+                <span>{c.quantity}</span>
+                <button onClick={() => incrementQuantity(c)}>+</button>
               </div>
             </li>
           ))}
         </ul>
+        {discount > 0 && (
+          <div className="movies__cart-total">
+            <p>You have a discount of: {discount}</p>
+          </div>
+        )}
         <div className="movies__cart-total">
           <p>Total: ${getTotal()}</p>
         </div>
       </div>
     </section>
   )
-} 
+}
--- src/components/pages/Exercise02/index.js
+++ src/components/pages/Exercise02/index.js
@@ -8,47 +8,82 @@
  * list of movies that belong to that gender (Filter all movies).
  * 3. Order the movies by year and implement a button that switch between ascending and descending order for the list
  * 4. Try to recreate the user interface that comes with the exercise (exercise02.png)
- * 
+ *
  * You can modify all the code, this component isn't well designed intentionally. You can redesign it as you need.
  */
 
-import "./assets/styles.css";
-import { useEffect, useState } from "react";
+import './assets/styles.css'
+import { useEffect, useState } from 'react'
 
-export default function Exercise02 () {
+export default function Exercise02() {
+  const [genres, setGenres] = useState([])
+  const [genre, setGenre] = useState('alls')
   const [movies, setMovies] = useState([])
   const [fetchCount, setFetchCount] = useState(0)
   const [loading, setLoading] = useState(false)
-
-  const handleMovieFetch = () => {
-    setLoading(true)
-    setFetchCount(fetchCount + 1)
-    console.log('Getting movies')
-    fetch('http://localhost:3001/movies?_limit=50')
-      .then(res => res.json())
-      .then(json => {
-        setMovies(json)
-        setLoading(false)
-      })
-      .catch(() => {
-        console.log('Run yarn movie-api for fake api')
-      })
-  }
+  const [order, setOrder] = useState('desc')
 
   useEffect(() => {
-    handleMovieFetch()
-  }, [handleMovieFetch])
+    const getGenres = async () => {
+      try {
+        const res = await fetch('http://localhost:3001/genres')
+        const json = await res.json()
+        setGenres(json.sort())
+      } catch (error) {
+        console.log('Run yarn movie-api for fake api')
+      }
+    }
+    getGenres()
+  }, [])
+
+  useEffect(() => {
+    const getMovies = async () => {
+      setLoading(true)
+      setFetchCount(fetchCount + 1)
+      try {
+        let url = `http://localhost:3001/movies?_page=1&_limit=50&_sort=year&_order=${order}`
+        if (genre !== 'alls') {
+          url = `http://localhost:3001/movies?genres_like=${genre}&_sort=year&_order=${order}`
+        }
+        const res = await fetch(url)
+        const json = await res.json()
+        setMovies(json)
+        setLoading(false)
+      } catch (error) {
+        console.log('Run yarn movie-api for fake api')
+      }
+    }
+    if (genre && genre !== '') {
+      getMovies()
+    }
+  }, [genre, order])
+
+  const onChangeGenre = evt => {
+    setGenre(evt.target.value)
+  }
+
+  const orderMovies = () => {
+    if (order === 'desc') {
+      setOrder('asc')
+    } else {
+      setOrder('desc')
+    }
+  }
 
   return (
     <section className="movie-library">
-      <h1 className="movie-library__title">
-        Movie Library
-      </h1>
+      <h1 className="movie-library__title">Movie Library</h1>
       <div className="movie-library__actions">
-        <select name="genre" placeholder="Search by genre...">
-          <option value="genre1">Genre 1</option>
+        <select name="genre" placeholder="Search by genre..." onChange={onChangeGenre}>
+          <option value="alls">Alls</option>
+          {genres.length > 0 &&
+            genres.map(g => (
+              <option key={g} value={g}>
+                {g}
+              </option>
+            ))}
         </select>
-        <button>Order Descending</button>
+        <button onClick={orderMovies}>Year {order === 'asc' ? 'Ascending' : 'Descending'}</button>
       </div>
       {loading ? (
         <div className="movie-library__loading">
@@ -58,15 +93,15 @@ export default function Exercise02 () {
       ) : (
         <ul className="movie-library__list">
           {movies.map(movie => (
-            <li key={movie.id} className="movie-library__card">
-              <img src={movie.posterUrl} alt={movie.title} />
-              <ul>
-                <li>ID: {movie.id}</li>
-                <li>Title: {movie.title}</li>
-                <li>Year: {movie.year}</li>
-                <li>Runtime: {movie.runtime}</li>
-                <li>Genres: {movie.genres.join(', ')}</li>
-              </ul>
+            <li key={movie.id}>
+              <div className="movie-library__card">
+                <img src={movie.posterUrl} alt={movie.title} />
+                <ul>
+                  <li className="movie-library__card-title">{movie.title}</li>
+                  <li className="movie-library__card-genres">{movie.genres.join(', ')}</li>
+                  <li className="movie-library__card-year">{movie.year}</li>
+                </ul>
+              </div>
             </li>
           ))}
         </ul>
